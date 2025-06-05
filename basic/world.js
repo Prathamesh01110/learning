0499a36a88edafa319acf25f19c294670851da31
Prathamesh Jakkula <prathamesh.jakkula.01042005@gmail.com>
2025-06-05 10:33:55 +0530

    i learned function

--- basic/world.js
+++ basic/world.js
@@ -19,6 +19,15 @@
 // const prompt = require('prompt-sync')();
 // const name = prompt("What is your name? ");
 // console.log("Hello, " + name + "!");
+// ctrl + ` opens vscode termianl
+
+
+function sq(x){
+    let ans = 0;
+    ans = x*x;
+    return ans;
+}
+console.log("the answer is: " + sq(12));
 
 
 
